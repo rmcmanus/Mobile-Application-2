461339528ddacbd6818e1c661371f7b73a44d731
Ryan McManus <rmcmanus@mines.edu>
2013-09-29 01:37:31 -0600

    Added function populateThrowHitSpinners to AddPlayerActivity class.  This populates two new spinners with "Right" and "Left" as spinner options. Added text to define what each spinner does. Added comments to functions that I wrote.

--- ApplicationTwo/src/edu/mines/rmcmanus/dhunter/applicationtwo/AddPlayerActivity.java
+++ ApplicationTwo/src/edu/mines/rmcmanus/dhunter/applicationtwo/AddPlayerActivity.java
@@ -14,8 +14,19 @@ public class AddPlayerActivity extends Activity {
 		setContentView(R.layout.activity_add_player);
 		populatePositionSpinner();
 		populateNumberSpinner();
+		populateThrowHitSpinners();
 	}
 
+	/**
+	 * Creates and populates a spinner that allows the user
+	 * to choose the position of their new player created.
+	 * This avoids error checking as the only choices that can
+	 * be made are those that are populated in the spinner.
+	 * 
+	 * Since there are a limited number of positions on the baseball diamond,
+	 * the names of each position are populated in a string-array in
+	 * the string.xml.
+	 */
 	public void populatePositionSpinner() {
 		Spinner positionSpinner = (Spinner) findViewById(R.id.position_edit_text);
 		ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.position_array, android.R.layout.simple_spinner_dropdown_item);
@@ -23,6 +34,18 @@ public class AddPlayerActivity extends Activity {
 		positionSpinner.setAdapter(arrayAdapter);
 	}
 	
+	/**
+	 * Creates and populates a spinner that allows the user
+	 * to choose the number of their new player created.
+	 * This avoids error checking as the only choices that can
+	 * be made are those that are populated in the spinner.
+	 * 
+	 * There are a limited number of team numbers that can be
+	 * assigned to a player. They range from 00-99. However,
+	 * instead of writing every string to an array, {@link #populateNumberSpinner()}
+	 * generates the strings by calling {@link #populateNumberArray(ArrayList)},
+	 * and then places the spinner.
+	 */
 	public void populateNumberSpinner() {
 		Spinner numberSpinner = (Spinner) findViewById(R.id.number_edit_text);
 		
@@ -34,6 +57,34 @@ public class AddPlayerActivity extends Activity {
 		numberSpinner.setAdapter(arrayAdapter);
 	}
 	
+	/**
+	 * Creates and populates a spinner that allows the user
+	 * to choose the hand with which the newly created player
+	 * throws and hits. This avoids error checking as the only
+	 * choices that can be made are populated in the spinner.
+	 * 
+	 * This function only allows 2 options: Right and Left.
+	 * Due to the similarity of both spinners, both were
+	 * populated simultaneously.
+	 */
+	public void populateThrowHitSpinners() {
+		Spinner throwSpinner = (Spinner) findViewById(R.id.throwing_edit_text);
+		Spinner hitSpinner = (Spinner) findViewById(R.id.hitting_edit_text);
+		
+		ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.direction_array, android.R.layout.simple_spinner_dropdown_item);
+		arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		throwSpinner.setAdapter(arrayAdapter);
+		hitSpinner.setAdapter(arrayAdapter);
+	}
+	
+	/**
+	 * Generates the numbers 00-99 as strings, and then populates the
+	 * passed ArrayList object.
+	 * 
+	 * @param numberArray 	The parameter passed to store numbers generated
+	 * @return 				Returns the array populated with strings
+	 */
 	public ArrayList<String> populateNumberArray(ArrayList<String> numberArray) {
 		for (int i = 0; i < 100; i++) {
 			if (i < 10) {
